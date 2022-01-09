127c6b26b8bdf005b2fee716b14fdf19e03a5e7d
Oguzhan <oguzhanmemis266@gmail.com>
2022-01-09 23:44:24 +0300

    screenshot was added by html2canvas

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import html2canvas from 'html2canvas';
+
 
 const app = createApp(App);
 app.use(router);
 app.mount('#app');
+app.use(html2canvas);
--- src/views/Video.vue
+++ src/views/Video.vue
@@ -1,25 +1,43 @@
+import VueHtml2Canvas from 'vue-html2canvas';
 <template>
-<div class="container">
-  <div class="row">
-    <div class="d-flex justify-content-center">
-      <section>
+  <div class="container">
+    <div class="row">
+      <div id="screenShot" class="d-flex justify-content-center">
+        <!-- <section>
         <iframe
           width="640"
           height="480"
           src="http://46.151.101.149:8081/?action=stream"
           frameborder="0"
         ></iframe>
-      </section>
+      </section> -->
+        <img src="../assets/cam.jpg" alt="cam" class="cam" />
+      </div>
+      <div  class="d-flex justify-content-center m-3">
+        <button @click="getScreenShot()">Ekran Görüntüsü Al</button>
+      </div>
     </div>
-      
   </div>
-</div>
-  
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    getScreenShot() {
+      const html2canvas = require('html2canvas');
+      console.log("ss alındı");
+      html2canvas(document.querySelector("#screenShot")).then(canvas => {
+      document.body.appendChild(canvas)
+});
+    },
+  },
+};
 </script>
 
 <style>
+.cam {
+  width: 640px;
+  height: 480px;
+  margin: 20px;
+}
 </style>
